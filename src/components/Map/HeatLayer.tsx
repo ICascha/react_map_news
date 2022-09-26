7e8e16cb13b61384381e7f58c310a35c6c811b5f
ICascha <caschavanwanrooij@gmail.com>
2022-09-26 14:49:43 +0200

    big commit :)

--- src/components/Map/HeatLayer.tsx
+++
@@ -1,9 +0,0 @@
-import { useMap } from "react-leaflet";
-
-function HeatLayer({ center, zoom }: ControlProps) {
-  const map = useMap();
-  map.add();
-  return null;
-}
-
-export default HeatLayer;
--- src/components/Map/MapComponent.tsx
+++ src/components/Map/MapComponent.tsx
@@ -1,8 +1,25 @@
 import { Avatar, Carousel, Divider, Drawer, List, Modal } from "antd";
 import { LatLngTuple } from "leaflet";
 import React from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  FeatureGroup,
+} from "react-leaflet";
 import ChangeView from "./ChangeView";
+import HeatLayer from "./HeatLayer";
+import DrawingControl from "./DrawingControl";
+import L from "leaflet";
+import testIcon from "./test.svg";
+
+const iconPerson = new L.Icon({
+  iconUrl: testIcon, //require("./test.svg").default,
+  iconRetinaUrl: testIcon, //require("./test.svg").default,
+  iconSize: new L.Point(20, 20),
+  // className: "leaflet-div-icon",
+});
 
 const somaliaPos: LatLngTuple = [5.152149, 46.199615];
 
@@ -65,6 +82,7 @@ const MapComponent = ({ center, zoom }: ControlProps) => {
       style={{ width: "100%", height: "100% " }}
     >
       <ChangeView center={center} zoom={zoom} />
+      <HeatLayer />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -74,6 +92,7 @@ const MapComponent = ({ center, zoom }: ControlProps) => {
         eventHandlers={{
           click: (i) => showDrawer(),
         }}
+        icon={iconPerson}
       ></Marker>
       {/* <Modal
         title="Basic Modal"
@@ -122,6 +141,7 @@ const MapComponent = ({ center, zoom }: ControlProps) => {
         />
       </Drawer>
       {/* </Modal> */}
+      <DrawingControl />
     </MapContainer>
   );
 };
