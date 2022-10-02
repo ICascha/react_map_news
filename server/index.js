92ea37a034f946df37f3c42b95321ea5271642d5
Cascha van Wanrooij <caschavanwanrooij@gmail.com>
2022-10-02 15:42:02 +0200

    updated packages

--- server/index.js
+++ server/index.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
+const mysql = require('mysql');
 
-app.get("/", (req, res) => res.send("Hello, world!!!"));
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'Pnqf@ju2',
+    database: 'news_analysis_db'
+});
+
+app.get("/", (req, res) => {
+    const sqlInsert = "INSERT INTO article_locations (id, lat, lng) VALUES (1, 3.31, 72.321);"
+    db.query(sqlInsert, (err, result) => {
+        console.log(err);
+        console.log(result);
+        res.send('hello, Cascha!')
+    })
+});
 
 app.listen(3001, () => console.log("running on port 3001"));
